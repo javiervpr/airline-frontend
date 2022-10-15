d616ed03bedcb727d46fb270e9a5f38bc7e21218
Nahim Terrazas Parada <nahimdhaney@hotmail.com>
2022-10-15 10:06:52 -0400

    adicionando pasajeros.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {FlightsPageComponent} from "./pages/flights-page/flights-page.component";
+import { PassangersPageComponent } from './pages/passangers-page/passangers-page.component';
 
 const routes: Routes = [
   {path: '', component: FlightsPageComponent},
-  {path: 'flights', component: FlightsPageComponent}
+  {path: 'flights', component: FlightsPageComponent},
+  {path: 'passangers', component: PassangersPageComponent}
 ];
 
 @NgModule({
