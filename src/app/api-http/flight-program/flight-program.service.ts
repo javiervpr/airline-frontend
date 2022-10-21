fa5ebffa7da037316354ec74619eef49f09953dd
Franz Bascope <fbascope@credibl.com>
2022-10-21 00:10:53 -0400

    add create for flight program

--- src/app/api-http/flight-program/flight-program.service.ts
+++ src/app/api-http/flight-program/flight-program.service.ts
@@ -1,22 +1,29 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from "@angular/common/http";
-import {FlightProgram} from "../../api-models";
-import {environment} from "../../../environments/environment";
+import { HttpClient } from '@angular/common/http';
+import { FlightProgram } from '../../api-models';
+import { environment } from '../../../environments/environment';
+import { builEndpoint } from '../utils/utils';
+import { flightProgramEndpoint } from '../utils/endpoints';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class FlightProgramService {
+  constructor(private http: HttpClient) {}
 
-  constructor(
-    private http: HttpClient
-  ) { }
+  flightProgramEndpoint = builEndpoint(flightProgramEndpoint);
 
   getAll() {
-    return this.http.get<FlightProgram[]>(`${environment.baseUrl}/api/flight_programs`);
+    return this.http.get<FlightProgram[]>(
+      `${environment.baseUrl}/api/flight_programs`
+    );
   }
 
   get() {
     return this.http.get<FlightProgram[]>(`${environment.baseUrl}/api/flight`);
+  }
+
+  create(data: any) {
+    return this.http.post<any>(this.flightProgramEndpoint, data);
   }
 }
--- src/app/pages/flight-program-form/flight-program-form.component.ts
+++ src/app/pages/flight-program-form/flight-program-form.component.ts
@@ -1,22 +1,41 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { lastValueFrom } from 'rxjs';
+import { FlightProgramService } from 'src/app/api-http/flight-program/flight-program.service';
 import { airportCodes } from 'src/app/utils/airport-codes';
 
 @Component({
   selector: 'app-flight-program-form',
   templateUrl: './flight-program-form.component.html',
-  styleUrls: ['./flight-program-form.component.scss']
+  styleUrls: ['./flight-program-form.component.scss'],
 })
 export class FlightProgramFormComponent implements OnInit {
+  flightProgramForm = this.formBuilder.group({
+    sourceAirport: ['', Validators.required],
+    destinyAirport: ['', Validators.required],
+    flightCode: ['', Validators.required],
+  });
+  submitted = false;
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private flightProgramsService: FlightProgramService
+  ) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  public get airportCodes(): string[] {
+    return airportCodes;
   }
 
-
-  public get airportCodes() : string[] {
-    return airportCodes
+  async onCreateSubmit() {
+    this.submitted = true;
+    if (this.flightProgramForm.valid) {
+      let data: any = this.flightProgramForm.value;
+      data.itineraryId = 1;
+      const respose = await lastValueFrom(
+        this.flightProgramsService.create(data)
+      );
+    }
   }
-
-
 }
